69232dfe7abdac83cddf9e32e0c279a93776d23d
Song Yang <songyang.dev@gmail.com>
2024-03-15 18:54:41 -0400

    Start segment loading

--- lib/ember_quest.dart
+++ lib/ember_quest.dart
@@ -2,7 +2,12 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-import 'package:flame_platformer_tutorial/actors/ember.dart';
+import 'actors/ember.dart';
+import 'actors/water_enemy.dart';
+import 'managers/segment_manager.dart';
+import 'objects/ground_block.dart';
+import 'objects/platform_block.dart';
+import 'objects/star.dart';
 
 class EmberQuestGame extends FlameGame {
   late EmberPlayer _ember;
@@ -21,8 +26,39 @@ class EmberQuestGame extends FlameGame {
 
     camera.viewfinder.anchor = Anchor.topLeft;
 
-    _ember = EmberPlayer(position: Vector2(128, canvasSize.y - 70));
+    initializeGame();
+  }
 
+  void initializeGame() {
+    // assume size of the viewport is < 3200
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGameSegments(
+        i, (640 * i).toDouble(), // 640 is the size of a segment in pixels
+      );
+    }
+
+    _ember = EmberPlayer(position: Vector2(128, canvasSize.y - 70));
     world.add(_ember);
+  }
+
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+          break;
+
+        case PlatformBlock:
+          break;
+
+        case Star:
+          break;
+
+        case WaterEnemy:
+          break;
+      }
+    }
   }
 }
