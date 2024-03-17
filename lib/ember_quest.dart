0654058c79591cbd4453c93a289fd3ddc851107d
Song Yang <songyang.dev@gmail.com>
2024-03-16 21:38:32 -0400

    Add star sprite

--- lib/ember_quest.dart
+++ lib/ember_quest.dart
@@ -60,6 +60,7 @@ class EmberQuestGame extends FlameGame {
           break;
 
         case Star:
+          add(Star(gridPosition: block.gridPosition, xOffset: xPositionOffset));
           break;
 
         case WaterEnemy:
